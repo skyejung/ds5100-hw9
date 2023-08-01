936a4a3990267272766d05c85f7e0004253a0339
skye <107228623+skyejung@users.noreply.github.com>
2023-07-31 20:13:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='ds5100 module 9 hw',
+      url='http://github.com/skyejung/ds5100-hw9',
+      author='Skye',
+      author_email='hsj5sn@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
